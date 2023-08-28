955bc3a3e4a987cf092e67a394f5270785408e0d
timothy <timothy.ndarau@student.moringaschool.com>
2023-08-28 18:36:04 +0300

    done but eii the test aint passing its contradicting me

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
-def write_file(file_name, file_content):
-    pass
+from distutils import text_file
 
-def append_file(file_name, append_content):
-    pass
+
+
+def write_file(file_name, file_content):
+    full_file_name = file_name + ".txt"
+    with open(full_file_name, mode= "w") as text_file:
+        text_file.write(file_content)
+
+def append_file(file_name, file_content):
+    full_file_name = file_name + ".txt"
+    with open(full_file_name, mode= "a") as text_file:
+        text_file.write(file_content)
 
 def read_file(file_name):
-    pass
+    full_file_name = file_name + ".txt"
+    with open(full_file_name, mode= "r") as text_file:
+        content = text_file.read()
+    return content
